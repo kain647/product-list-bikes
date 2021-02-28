b213ddaeaf62f4577cf22d53c8fd83e630c42d0b
Aleksey <aleksey.amelchenko@gmail.com>
2021-02-28 14:16:02 +0300

    Initialize project using Create React App

--- src/product-list/styles.js
+++ src/product-list/styles.js
@@ -79,7 +79,7 @@ export const Cover = styled.div`
   font-weight: 500;
   font-size: 48px;
   font-family: "Space Grotesk", sans-serif;
-	word-wrap: break-word;
+  word-wrap: break-word;
 `;
 export const Product = styled.div`
   display: flex;
@@ -141,16 +141,173 @@ export const Price = styled.div`
   margin-bottom: 30px;
 `;
 export const Subtitle = styled.div`
+  display: -webkit-box;
+  -webkit-line-clamp: 8;
+  -webkit-box-orient: vertical;
   color: #414041;
   line-height: 1.5em;
   font-size: 14px;
-  margin-bottom: 20px;
+  overflow: hidden;
+	margin-bottom: auto;
 `;
-export const More = styled.button`
-  margin-top: auto;
+export const DetailButton = styled.button`
   font-weight: 500;
   font-size: 14px;
   letter-spacing: 1px;
   padding: 10px;
+  text-transform: uppercase;
   cursor: pointer;
+`;
+export const ModalContainer = styled.div`
+  display: flex;
+  box-sizing: border-box;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 768px;
+`;
+export const ModalHeader = styled.div`
+  display: flex;
+  position: sticky;
+  margin-left: auto;
+  padding: 25px;
+  top: 0;
+  z-index: 1;
+  svg {
+    --size: 36px;
+    width: var(--size);
+    height: var(--size);
+    color: #fff;
+    border-radius: 50%;
+    background-color: #00000080;
+    cursor: pointer;
+  }
+`;
+export const ProductNew = styled.h3`
+  display: flex;
+  text-transform: capitalize;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  padding: 0;
+  margin: 0;
+`;
+export const ProductImage = styled.div`
+  display: flex;
+  margin-bottom: 40px;
+`;
+export const ProductBrand = styled.div`
+  display: flex;
+  text-transform: uppercase;
+  font-size: 16px;
+  letter-spacing: 1px;
+`;
+export const ProductModalTitle = styled.div`
+  display: flex;
+  font-size: 44px;
+  max-width: none;
+  font-weight: 500;
+  margin: 20px 0;
+`;
+export const ProductPrice = styled.div`
+  display: flex;
+  font-size: 24px;
+  margin-bottom: 30px;
+`;
+export const ProductButtons = styled.div`
+  display: flex;
+  align-items: center;
+`;
+export const ProductOptions = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 15px;
+`;
+export const ButtonCart = styled.button`
+  display: flex;
+  text-transform: uppercase;
+  color: #fff;
+  border-radius: 4px;
+  background-color: #fc302b;
+  border: 1px solid #fc302b;
+  padding: 12px 20px;
+  font-size: 14px;
+  font-weight: 500;
+  letter-spacing: 1px;
+  cursor: pointer;
+`;
+export const ProductOption = styled.button`
+  display: flex;
+  text-transform: uppercase;
+  background-color: transparent;
+  border: 1px solid #08090a;
+  padding: 12px 20px;
+  font-size: 14px;
+  font-weight: 500;
+  letter-spacing: 1px;
+  margin-right: -1px;
+  cursor: pointer;
+  &.ProductTable {
+    display: flex;
+    width: 70%;
+    justify-content: center;
+    border-color: #323132;
+    border-radius: 4px;
+    margin: 25px auto;
+    background-color: #323132;
+    color: #fff;
+  }
+  &.active {
+    background-color: #08090a;
+    color: #fff;
+    border-color: #08090a;
+  }
+`;
+export const ProductSubtitle = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 20px 70px;
+  color: #414041;
+  line-height: 1.5em;
+  font-size: 14px;
+`;
+export const ProductDetailImage = styled.div`
+  display: flex;
+  img {
+    width: 100%;
+    height: auto;
+  }
+`;
+export const TableRow = styled.tr`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 20px;
+  border-top: 1px solid rgba(255, 255, 255, 0.1);
+  padding: 12px;
+`;
+export const CellHead = styled.th`
+  display: flex;
+  justify-content: left;
+  align-items: center;
+  font-weight: 600;
+  letter-spacing: 1px;
+  font-size: 20px;
+  margin-bottom: 30px;
+`;
+export const Cell = styled.td`
+  display: flex;
+  width: 100%;
+  &.name {
+    text-transform: uppercase;
+  }
+`;
+export const ProductGrid = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  img {
+    width: 33.333%;
+    max-height: 320px;
+    object-fit: cover;
+  }
 `;
