9e13c8394e2cce27ae3699f8dc276724eaa064c1
Aleksey <aleksey.amelchenko@gmail.com>
2021-03-02 00:05:52 +0300

    Initialize project using Create React App

--- src/product-list/index.js
+++ src/product-list/index.js
@@ -1,14 +1,8 @@
 import React from "react";
 import ModalBike from "./modal";
-import bg from "../upload/background.jpg";
-import marlin from "../upload/Marlin6Women.jpg";
-import levo from "../upload/Slash98XT.jpg";
-import powerfly from "../upload/Slash7NX.jpg";
-import jumper from "../upload/TopFuel7SX.jpg";
-import frameset from "../upload/XCaliber9.jpg";
-import precaliber from "../upload/Precaliber.jpg";
-import { ReactComponent as Logo } from "../upload/logo.svg";
-import { ReactComponent as Cart } from "../upload/cart.svg";
+import { bikes } from "./data";
+import { ReactComponent as Logo } from "../icons/logo.svg";
+import { ReactComponent as Cart } from "../icons/cart.svg";
 import {
   Container,
   Nav,
@@ -24,66 +18,10 @@ import {
   BrandBike,
   ProductTitle,
   Price,
-  Subtitle,
+  Subtitle
 } from "./styles";
 
 const Bikes = props => {
-  const bikes = [
-    {
-      title: "new",
-      photo: marlin,
-      brand: "epic",
-      productTitle: "marlin 6 women's",
-      price: "$699.99",
-      subtitle:
-        "Marlin 6 Women's is a cross country mountain bike built to give you an efficient off-road ride with a suspension fork that locks out, and smoother-shifting 2x8 drivetrain. It's made for mountain biking, but it's also equipped with features like rack and kickstand mounts that make it a great choice for adventurous daily commutes.",
-    },
-    {
-      title: "new",
-      photo: levo,
-      brand: "turbo levo",
-      productTitle: "Turbo Levo SL Expert Carbon",
-      price: "$9,025",
-      subtitle:
-        "The Turbo Levo SL is a new, lightweight breed of eMTB that harnesses the quick and lively ride of our Stumpjumper...and adds just enough power to introduce a whole new dimension of kick ass. The Turbo Levo SL Expert Carbon features a full carbon frame paired to full FOX suspension and a SRAM Eagle GX drivetrain.",
-    },
-    {
-      title: "new",
-      photo: powerfly,
-      brand: "enduro",
-      productTitle: "Powerfly FS 9 Equipped",
-      price: "$5,999.99",
-      subtitle:
-        "Powerfly FS 9 EQ is a full suspension electric mountain bike that’s built for singletrack sessions and rugged commutes. You get high-end parts, a powerful drive system, and a rad suspension setup for tackling rougher trails. And, it comes with utility-focused features for daily riding, like fenders, lights, and a rear rack.",
-    },
-    {
-      title: "new",
-      photo: jumper,
-      brand: "STUMPJUMPER",
-      productTitle: "Enduro S-WORKS 2020",
-      price: "$9,750",
-      subtitle:
-        "This is the bike that defines the very genre that bears its name. The S-Works Enduro features a bomber-yet-lightweight carbon frame that blissfully tackles the kind of terrain that chews up and spits out lesser bikes. And it's absolutely dressed to kill. Let the drooling begin.",
-    },
-    {
-      title: "new",
-      photo: frameset,
-      brand: "TURBO LEVO",
-      productTitle: "Ticket DJ Frameset",
-      price: "$879.99",
-      subtitle:
-        "Ticket DJ is a lightweight, durable dirt jump mountain bike frame with a compact rear end and roomy top tube for major style, the best handling, and the biggest tricks. Horizontal sliding dropouts let you choose between a singlespeed or geared setup for a personalized ride.",
-    },
-    {
-      title: "new",
-      photo: precaliber,
-      brand: "epic",
-      productTitle: "Precaliber Suspension",
-      price: "$389.99",
-      subtitle:
-        "Precaliber 24 8-Speed is a versatile kids' bike built for young adventurers who love riding off the pavement and into the dirt. It has a sturdy yet light aluminum frame with a performance suspension fork and an 8-speed drivetrain perfect for racing home from school, zipping through the woods.",
-    }
-  ];
   return (
     <Container>
       <Nav>
@@ -96,11 +34,11 @@ const Bikes = props => {
           <Cart className="Cart" />
         </NavCart>
       </Nav>
-      <Cover bg={bg}>
+      <Cover>
         Mountain <br /> Bikes
       </Cover>
       <Product>
-        {bikes.map(bike => {
+        {Object.values(bikes).map(bike => {
           return <Bike {...bike} />;
         })}
       </Product>
@@ -109,18 +47,18 @@ const Bikes = props => {
 };
 
 const Bike = props => {
-  const { title, photo, brand, productTitle, price, subtitle } = props;
+  const { title, photo, type, productTitle, price, subtitle, id } = props;
   return (
     <ProductContainer>
       <Title>{title}</Title>
       <PhotoBike className="PhotoBike">
-        <img src={photo} />
+        <img src={`/images/${photo}`} />
       </PhotoBike>
-      <BrandBike>{brand}</BrandBike>
+      <BrandBike>{type}</BrandBike>
       <ProductTitle>{productTitle}</ProductTitle>
       <Price>{price}</Price>
       <Subtitle>{subtitle}</Subtitle>
-      <ModalBike />
+      <ModalBike id={id} />
     </ProductContainer>
   );
 };
